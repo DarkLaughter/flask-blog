db1ea6cf1951c8ae72782d5b835a30fb8f1d1a02
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-01-17 17:39:00 -0500

    syxtax fixes, base html fix

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,13 +1,13 @@
-from flask import render_temlate, request, Blueprint
+from flask import render_template, request, Blueprint
 
 core = Blueprint('core', __name__)
 
 
 @core.route('/')
 def index():
-    return render_temlate('index.html')
+    return render_template('index.html')
 
 
 @core.route('/info')
 def info():
-    return render_temlate('info.html')
+    return render_template('info.html')
