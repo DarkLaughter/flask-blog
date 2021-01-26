a283010b9f494314e59e1fbf3e7903901b96dcdf
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-01-26 09:45:09 -0500

    post/blog forms

---
+++ blog/blog_posts/forms.py
@@ -0,0 +1,9 @@
+from flask import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+class BlogPostForm(FlaskForm):
+    titile = StringField('Title',validators=[DataRequired()])
+    text = TextAreaField('Text',validators=[DataRequired()])
+    submit = SubmitField("Post Blog")
+    
