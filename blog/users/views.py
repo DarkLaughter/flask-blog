cf7e530678731f68663ebcb1b8a097314528c687
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-01-25 21:19:42 -0500

    posts route/logic

--- blog/users/views.py
+++ blog/users/views.py
@@ -77,4 +77,10 @@ def account():
     return render_template('account.html',profile_image=profile_image,form=form)
 
 
-# Users list of Blog Posts
+# list of Blog Posts
+@users.route("/<username>")
+def user_posts(username):
+    page = request.args.get('page',1,type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = Post.query.filter_by(author=user).order_by(Post.date.desc()).paginate(page=page,per_page=5)
+    return render_template('user_blog_posts.html',blog_posts=blog_posts,user=user)
