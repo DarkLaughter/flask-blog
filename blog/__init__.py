708ec6c7f200b2357950c94f1c2101e9310ac369
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-01-26 12:49:29 -0500

    post views and forms added

--- blog/__init__.py
+++ blog/__init__.py
@@ -7,7 +7,7 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 
+app.config['SECRET_KEY'] = 'mysecret'
 
 ###########
 ###DB#####
@@ -27,10 +27,12 @@ login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
 ######
-from blog.users.views import users
-from blog.error_pages.handlers import error_pages
 from blog.core.views import core
+from blog.error_pages.handlers import error_pages
+from blog.users.views import users
+from blog.blog_posts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
